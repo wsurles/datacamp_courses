f342b294c10b23474bdf90fd9298e27733b88dc3
William Surles <williamsurles@gmail.com>
2017-08-02 10:59:07 -0400

    ggplot2 part2 notebook complete, and some updates to part1

---
+++ courses/data_visualization_with_ggplot2_part_2/create_datasets.R
@@ -0,0 +1,78 @@
+
+
+mamsleep <- structure(list(vore = c("omni", "herbi", "omni", "herbi", "herbi", 
+                                    "carni", "carni", "herbi", "herbi", "omni", "herbi", "omni", 
+                                    "omni", "omni", "carni", "herbi", "omni", "insecti", "herbi", 
+                                    "herbi", "omni", "omni", "carni", "omni", "herbi", "carni", "carni", 
+                                    "herbi", "omni", "herbi", "carni", "omni", "herbi", "herbi", 
+                                    "herbi", "insecti", "herbi", "herbi", "herbi", "omni", "omni", 
+                                    "carni", "herbi", "insecti", "herbi", "omni", "insecti", "herbi", 
+                                    "herbi", "herbi", "omni", "herbi", "omni", "omni", "carni", "carni", 
+                                    "omni", "herbi", "omni", "herbi", "herbi", "carni", "carni", 
+                                    "herbi", "herbi", "omni", "herbi", "omni", "omni", "omni", "carni", 
+                                    "herbi", "omni", "insecti", "herbi", "herbi", "omni", "omni", 
+                                    "carni", "omni", "herbi", "carni", "carni", "herbi", "omni", 
+                                    "herbi", "carni", "omni", "herbi", "herbi", "herbi", "insecti", 
+                                    "herbi", "herbi", "herbi", "omni", "omni", "carni", "herbi", 
+                                    "insecti", "herbi", "omni", "insecti", "herbi", "herbi", "herbi", 
+                                    "omni", "herbi", "omni", "omni", "carni", "carni"), name = c("Owl monkey", 
+                                                                                                 "Mountain beaver", "Greater short-tailed shrew", "Cow", "Three-toed sloth", 
+                                                                                                 "Northern fur seal", "Dog", "Goat", "Guinea pig", "Grivet", "Chinchilla", 
+                                                                                                 "Star-nosed mole", "African giant pouched rat", "Lesser short-tailed shrew", 
+                                                                                                 "Long-nosed armadillo", "Tree hyrax", "North American Opossum", 
+                                                                                                 "Big brown bat", "Horse", "Donkey", "European hedgehog", "Patas monkey", 
+                                                                                                 "Domestic cat", "Galago", "Giraffe", "Pilot whale", "Gray seal", 
+                                                                                                 "Gray hyrax", "Human", "Mongoose lemur", "Thick-tailed opposum", 
+                                                                                                 "Macaque", "Mongolian gerbil", "Golden hamster", "House mouse", 
+                                                                                                 "Little brown bat", "Degu", "Rabbit", "Sheep", "Chimpanzee", 
+                                                                                                 "Baboon", "Caspian seal", "Potoroo", "Giant armadillo", "Laboratory rat", 
+                                                                                                 "Squirrel monkey", "Eastern american mole", "Cotton rat", "Thirteen-lined ground squirrel", 
+                                                                                                 "Golden-mantled ground squirrel", "Pig", "Brazilian tapir", "Tenrec", 
+                                                                                                 "Tree shrew", "Genet", "Red fox", "Owl monkey", "Mountain beaver", 
+                                                                                                 "Greater short-tailed shrew", "Cow", "Three-toed sloth", "Northern fur seal", 
+                                                                                                 "Dog", "Goat", "Guinea pig", "Grivet", "Chinchilla", "Star-nosed mole", 
+                                                                                                 "African giant pouched rat", "Lesser short-tailed shrew", "Long-nosed armadillo", 
+                                                                                                 "Tree hyrax", "North American Opossum", "Big brown bat", "Horse", 
+                                                                                                 "Donkey", "European hedgehog", "Patas monkey", "Domestic cat", 
+                                                                                                 "Galago", "Giraffe", "Pilot whale", "Gray seal", "Gray hyrax", 
+                                                                                                 "Human", "Mongoose lemur", "Thick-tailed opposum", "Macaque", 
+                                                                                                 "Mongolian gerbil", "Golden hamster", "House mouse", "Little brown bat", 
+                                                                                                 "Degu", "Rabbit", "Sheep", "Chimpanzee", "Baboon", "Caspian seal", 
+                                                                                                 "Potoroo", "Giant armadillo", "Laboratory rat", "Squirrel monkey", 
+                                                                                                 "Eastern american mole", "Cotton rat", "Thirteen-lined ground squirrel", 
+                                                                                                 "Golden-mantled ground squirrel", "Pig", "Brazilian tapir", "Tenrec", 
+                                                                                                 "Tree shrew", "Genet", "Red fox"), sleep = structure(c(1L, 1L, 
+                                                                                                                                                        1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 
+                                                                                                                                                        1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 
+                                                                                                                                                        1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 
+                                                                                                                                                        1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 
+                                                                                                                                                        2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 
+                                                                                                                                                        2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 
+                                                                                                                                                        2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L), .Label = c("total", 
+                                                                                                                                                                                                                            "rem"), class = "factor"), time = c(17, 14.4, 14.9, 4, 14.4, 
+                                                                                                                                                                                                                                                                8.7, 10.1, 5.3, 9.4, 10, 12.5, 10.3, 8.3, 9.1, 17.4, 5.3, 18, 
+                                                                                                                                                                                                                                                                19.7, 2.9, 3.1, 10.1, 10.9, 12.5, 9.8, 1.9, 2.7, 6.2, 6.3, 8, 
+                                                                                                                                                                                                                                                                9.5, 19.4, 10.1, 14.2, 14.3, 12.5, 19.9, 7.7, 8.4, 3.8, 9.7, 
+                                                                                                                                                                                                                                                                9.4, 3.5, 11.1, 18.1, 13, 9.6, 8.4, 11.3, 13.8, 15.9, 9.1, 4.4, 
+                                                                                                                                                                                                                                                                15.6, 8.9, 6.3, 9.8, 1.8, 2.4, 2.3, 0.7, 2.2, 1.4, 2.9, 0.6, 
+                                                                                                                                                                                                                                                                0.8, 0.7, 1.5, 2.2, 2, 1.4, 3.1, 0.5, 4.9, 3.9, 0.6, 0.4, 3.5, 
+                                                                                                                                                                                                                                                                1.1, 3.2, 1.1, 0.4, 0.1, 1.5, 0.6, 1.9, 0.9, 6.6, 1.2, 1.9, 3.1, 
+                                                                                                                                                                                                                                                                1.4, 2, 0.9, 0.9, 0.6, 1.4, 1, 0.4, 1.5, 6.1, 2.4, 1.4, 2.1, 
+                                                                                                                                                                                                                                                                1.1, 3.4, 3, 2.4, 1, 2.3, 2.6, 1.3, 2.4)), row.names = c(NA, 
+                                                                                                                                                                                                                                                                                                                         -112L), .Names = c("vore", "name", "sleep", "time"), class = "data.frame")
+
+##----------------------------------------------------
+
+mtcars.cyl <- structure(list(cyl = c(4, 6, 8), wt.avg = c(2.28572727272727, 
+                                                          3.11714285714286, 3.99921428571429), sd = c(0.569563708624258, 
+                                                                                                      0.356345538567754, 0.759404744476927), n = c(11L, 7L, 14L), prop = c(0.34375, 
+                                                                                                                                                                           0.21875, 0.4375)), class = "data.frame", .Names = c("cyl", "wt.avg", 
+                                                                                                                                                                                                                               "sd", "n", "prop"), row.names = c(NA, -3L))
+
+##---------------------------------------
+
+## I already printed this here and saved it as an .Rds file
+## the file on the datacamp course page is unavailable so this is my work around to create the notebook
+adult <- readRDS('data/adult.Rds')
+
+##-------------------------------------------------------
