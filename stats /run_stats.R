03481578845ef938484c9445e90544d8692796ff
William Surles <williamsurles@gmail.com>
2017-10-20 21:36:40 -0600

    stats update

--- stats /run_stats.R
+++ stats /run_stats.R
@@ -1,6 +1,7 @@
 
 library(googlesheets)
 library(rmarkdown)
+library(dplyr)
 
 ##| Get data from google spreadsheet
 
@@ -13,6 +14,10 @@ gs_ws_ls(gs)
 # load data from worksheet
 df_william <- gs_read(ss = gs, ws = "william")
 saveRDS(df_william, file = "data_william")
+
+df_william %>% 
+  mutate(date = as.Date(Date, '%m/%d/%Y')) %>%
+  filter(date >= today() - 7 & date <= today())
 
 df_russell <- gs_read(ss = gs, ws = "russell")
 saveRDS(df_russell, file = "data_russell")
